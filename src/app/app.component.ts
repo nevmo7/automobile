6b881caba89021f0db0eff342abf261be20fd5a4
nevmo7 <mstha1205@gmail.com>
2024-09-13 13:25:47 +1000

    export fixed. filters fixed. sorting error fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { ItemService } from './services/item.service';
 import { ItemsInterface } from './types/items.interface';
 import { CommonModule } from '@angular/common';
+import { AppService } from './app.service';
 
 import { FormGroup, FormControl } from "@angular/forms";
 import { ngxCsv } from 'ngx-csv/ngx-csv';
@@ -16,11 +17,20 @@ import { ngxCsv } from 'ngx-csv/ngx-csv';
 })
 
 export class AppComponent implements OnInit {
+
+  constructor(private appService:AppService) {  }
   
   title = 'automobile';
   vehicles: Array<ItemsInterface> = [];
 
   itemsFirebaseService = inject(ItemService)
+
+  searchTerm: string = ""; 
+  filterName: string = "";
+  filterBy: string = "";
+  filterValue: string = "";
+  sortBy: string = "";
+  sortOrder: string = "";
 
   searchForm = new FormGroup({
     searchTerm: new FormControl(),
@@ -28,27 +38,47 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
 
-    const localfilterName = localStorage.getItem("filtername");
-    const localfilterBy = localStorage.getItem("filterby");
-    const localfilterValue = localStorage.getItem("filtervalue");
+    this.searchTerm = localStorage.getItem("searchName")!;
+    this.filterName = localStorage.getItem("filtername")!;
+    this.filterBy = localStorage.getItem("filterby")!;
+    this.filterValue = localStorage.getItem("filtervalue")!;
+    this.sortBy = localStorage.getItem("sortBy")!;
+    this.sortOrder = localStorage.getItem("sortOrder")!;
     
     //if there are filter properties in local storage, get filtered items
-    if (localfilterName != null && localfilterBy != null && localfilterValue != null) {
-      //getting filtered items with filter propertirs in local storage
-      this.filter(localfilterName, localfilterBy, localfilterValue)
-    }else{
+    if (this.searchTerm != null && this.filterName != null && this.filterBy != null && this.filterValue != null) {
+      console.log("filters found!! " + this.searchTerm + this.filterName + this.filterBy + this.filterValue + this.sortBy + this.sortOrder)
+      //getting filtered items with filter properties in local storage
+      this.itemsFirebaseService.getItemByFilter(this.searchTerm, this.filterName, this.filterBy, parseInt(this.filterValue))
+      .subscribe(items => {
+        this.vehicles = items;
+        if (this.sortBy != null && this.sortOrder != null) {
+          this.sort(this.sortBy, this.sortOrder)
+          console.log(items)
+        }
+      })
+    }else if(this.searchTerm != null){
+      this.itemsFirebaseService.getItemByName(this.searchTerm).subscribe(items => {
+        this.vehicles = items;
+        if (this.sortBy != null && this.sortOrder != null) {
+          this.sort(this.sortBy, this.sortOrder)
+          console.log(items)
+        }
+      })
+    } else{
       // getting all items from firestore
       this.itemsFirebaseService.getItems().subscribe(items => {
         this.vehicles = items;
       })
     }
-    
   }
 
   //search function to call the search service in firestore
   search(value: string): void {
+    this.searchTerm = value
     this.itemsFirebaseService.getItemByName(value).subscribe(items => {
       this.vehicles = items
+      localStorage.setItem("searchName", value);
     });
   }
 
@@ -59,9 +89,13 @@ export class AppComponent implements OnInit {
       localStorage.setItem("filtername", filterName);
       localStorage.setItem("filterby", filterBy);
       localStorage.setItem("filtervalue", filterValue);
+    }else{
+      alert("please give a value to filter")
     }
-
-    this.itemsFirebaseService.getItemByFilter(filterName, filterBy, parseInt(filterValue)).subscribe(items => {
+    
+    this.itemsFirebaseService.getItemByFilter(this.searchTerm, filterName, filterBy, parseInt(filterValue)).subscribe(items => {
+      
+      console.log("getting filtered data");
       this.vehicles = items
     })
   }
@@ -76,7 +110,9 @@ export class AppComponent implements OnInit {
   //sort function
   sort(sortBy: string, sortOrder: string){
     //store sort properties into local storage
-
+    
+    localStorage.setItem("sortBy", sortBy);
+    localStorage.setItem("sortOrder", sortOrder);
     if (sortOrder.match("ascending")) {
       console.log("sorting in ascending order")
       this.sortAscending(sortBy);
@@ -91,7 +127,15 @@ export class AppComponent implements OnInit {
   sortAscending(sortBy: string): void{
     if (this.vehicles !== null) {
       if (sortBy.match("name")){ //sort by name
-        this.vehicles.sort((a,b) => a.name < b.name ? -1 : 1)
+        this.vehicles.sort((a,b) => {
+          if (a.name < b.name) {
+            return -1;
+          }
+          if (a.name > b.name) {
+            return 1;
+          }
+          return -1;
+        });
       } else if (sortBy.match("acceleration")){ //sort by acceleration
         this.vehicles.sort((a,b) => a.acceleration < b.acceleration ? -1 : 1)
       } else if (sortBy.match("cylinders")){ //sort by cylinders
@@ -105,7 +149,20 @@ export class AppComponent implements OnInit {
       } else if (sortBy.match("origin")){ //sort by origin
         this.vehicles.sort((a,b) => a.origin < b.origin ? -1 : 1)
       } else if (sortBy.match("horsepower")){ //sort by horsepower
-        this.vehicles.sort((a,b) => a.horsepower < b.horsepower ? -1 : 1)
+        this.vehicles
+        .sort((a,b) => {
+          if (isNaN(a.horsepower)) {
+            return 1;
+          }else if (isNaN(b.horsepower)) {
+            return -1;
+          }else if (a.horsepower < b.horsepower) {
+            return -1;
+          }else if (a.horsepower > b.horsepower){
+            return 1;
+          }else{
+            return 0;
+          }
+        })
       } else if (sortBy.match("weight")){ //sort by weight
         this.vehicles.sort((a,b) => a.weight < b.weight ? -1 : 1)
       } 
@@ -131,7 +188,20 @@ export class AppComponent implements OnInit {
       } else if (sortBy.match("origin")){ //sort by origin
         this.vehicles.sort((a,b) => a.origin > b.origin ? -1 : 1)
       } else if (sortBy.match("horsepower")){ //sort by horsepower
-        this.vehicles.sort((a,b) => a.horsepower > b.horsepower ? -1 : 1)
+        this.vehicles
+        .sort((a,b) => {
+          if (isNaN(a.horsepower)) {
+            return 1;
+          }else if (isNaN(b.horsepower)) {
+            return -1;
+          }else if (a.horsepower > b.horsepower) {
+            return -1;
+          }else if (a.horsepower < b.horsepower){
+            return 1;
+          }else{
+            return 0;
+          }
+        })
       } else if (sortBy.match("weight")){ //sort by weight
         this.vehicles.sort((a,b) => a.weight > b.weight ? -1 : 1)
       } 
@@ -140,21 +210,7 @@ export class AppComponent implements OnInit {
 
   //function to export table to csv
   downloadFile(): void {
-    var options = { 
-      fieldSeparator: ',',
-      quoteStrings: '"',
-      decimalseparator: '.',
-      showLabels: false, 
-      showTitle: false,
-      title: 'Your title',
-      useBom: false,
-      noDownload: false,
-      headers: ["Name", "Acceleration", "Cylinders", "Displacement", "Year", "MGP", "Origin", "Horsepower", "Weight"]
-    }
-    console.log(JSON.stringify(this.vehicles));
-    new ngxCsv(JSON.stringify(this.vehicles), 'My Report', options);
-
-
+    this.appService.downloadFile(this.vehicles, 'vehicles_data');
   }
 }
 
--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -1,4 +1,5 @@
 import { inject, Injectable } from '@angular/core';
+// import db from '@angular/fire/firestore';
 import { collection, query, collectionData, Firestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { ItemsInterface } from '../types/items.interface';
@@ -9,34 +10,32 @@ import { where } from 'firebase/firestore';
 })
 export class ItemService {
   firestore = inject(Firestore)
-  itemsCollection = collection(this.firestore, "vehicles")
+  itemsCollection = collection(this.firestore, "vehicles");
 
   //actual function to get items from firestore
   getItems(): Observable<ItemsInterface[]> {
+
     return collectionData(query(this.itemsCollection)) as Observable<ItemsInterface[]>;
   }
 
   //search items by name
   getItemByName(name: string): Observable<ItemsInterface[]> {
     console.log(name)
-    return collectionData(query(this.itemsCollection, where("name", "==", name))) as Observable<ItemsInterface[]>;
+    return collectionData(query(this.itemsCollection, where("name", ">=", name), where("name", "<=", name + "\uf8ff"))) as Observable<ItemsInterface[]>;
   }
 
   //get items by filter with numbers
-  getItemByFilter(filterName: string, filterBy: string, value: number): Observable<ItemsInterface[]> {
-    console.log("filter name" + filterName)
-    console.log("filter by" + filterBy)
-    console.log("filter value" + value)
+  getItemByFilter(searchTerm: string, filterName: string, filterBy: string, value: number): Observable<ItemsInterface[]> {
     
     if (filterBy.match("<")) {
       //filter by less than
-      return collectionData(query(this.itemsCollection, where(filterName, "<", value))) as Observable<ItemsInterface[]>;
+      return collectionData(query(this.itemsCollection, where(filterName, "<", value), where("name", ">=", searchTerm), where("name", "<=", searchTerm + "\uf8ff"))) as Observable<ItemsInterface[]>;
     } else if (filterBy.match("=")) {
       //filter by equal to
-      return collectionData(query(this.itemsCollection, where(filterName, "==", value))) as Observable<ItemsInterface[]>;
+      return collectionData(query(this.itemsCollection, where(filterName, "==", value), where("name", ">=", searchTerm), where("name", "<=", searchTerm + "\uf8ff"))) as Observable<ItemsInterface[]>;
     } else {
       //filter by more than
-      return collectionData(query(this.itemsCollection, where(filterName, ">", value))) as Observable<ItemsInterface[]>;
+      return collectionData(query(this.itemsCollection, where(filterName, ">", value), where("name", ">=", searchTerm), where("name", "<=", searchTerm + "\uf8ff"))) as Observable<ItemsInterface[]>;
     }
   }
 
